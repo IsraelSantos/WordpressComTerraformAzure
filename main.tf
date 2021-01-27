62c07d051da37e99485065ba3e0c704d6c3cf6e8
Israel Santos <israel.santos.cr@gmail.com>
2021-01-26 23:13:22 -0300

    Testes para criacao de IP publico.

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ resource "azurerm_resource_group" "meu_grupo_de_recursos" {
   location = "Brazil South"
 }
 
-
 resource "azurerm_virtual_network" "minha_rede" {
   name                = "minha_rede"
   resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
@@ -21,6 +20,31 @@ resource "azurerm_subnet" "subnet_interna" {
   resource_group_name  = azurerm_resource_group.meu_grupo_de_recursos.name
   virtual_network_name = azurerm_virtual_network.minha_rede.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "meu_PIP" {
+  name                = "meu-PIP"
+  location            = azurerm_resource_group.meu_grupo_de_recursos.location
+  resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
+  allocation_method   = "Dynamic"
+  idle_timeout_in_minutes = 30
+
+  tags = {
+    environment = "Dev"
+  }
+}
+
+resource "azurerm_network_interface" "interface" {
+  name                = "test-nic"
+  location            = azurerm_resource_group.meu_grupo_de_recursos.location
+  resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
+
+  ip_configuration {
+    name                          = "configuration1"
+    subnet_id                     = azurerm_subnet.subnet_interna.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.meu_PIP.id
+  }
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "minha_maquina_virtual" {
@@ -59,3 +83,14 @@ resource "azurerm_linux_virtual_machine_scale_set" "minha_maquina_virtual" {
     }
   }
 }
+
+data "azurerm_public_ip" "meu_PIP" {
+  name                = azurerm_public_ip.meu_PIP.name
+  resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
+}
+
+output "public_ip_address" {
+  value = data.azurerm_public_ip.meu_PIP.ip_address
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_machine_scale_set
