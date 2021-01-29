c8175007ca0b8f9cc2bdea33d0bc3784e44b5568
Israel Santos <israel.santos.cr@gmail.com>
2021-01-29 02:20:52 -0300

    Criando máquina para o MySQL. Ainda não acabada.

--- main.tf
+++ main.tf
@@ -34,6 +34,18 @@ resource "azurerm_public_ip" "meu_PIP" {
   }
 }
 
+resource "azurerm_public_ip" "mysql_PIP" {
+  name                = "mysql-PIP"
+  location            = azurerm_resource_group.meu_grupo_de_recursos.location
+  resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
+  allocation_method   = "Dynamic"
+  idle_timeout_in_minutes = 30
+
+  tags = {
+    environment = "Dev"
+  }
+}
+
 resource "azurerm_lb" "meu_loadbalance" {
   name                = "test"
   location            = azurerm_resource_group.meu_grupo_de_recursos.location
@@ -71,8 +83,8 @@ resource "azurerm_lb_probe" "meu_http_probe" {
   port                = 80
 }
 
-resource "azurerm_virtual_machine_scale_set" "minha_maquina_virtual" {
-  name                = "MinhaMaquinaVirtual"
+resource "azurerm_virtual_machine_scale_set" "meu_conjunto_de_maquinas_wordpress" {
+  name                = "MeuConjuntoDeMaquinasWordpress"
   resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
   location            = azurerm_resource_group.meu_grupo_de_recursos.location
   
@@ -137,6 +149,49 @@ resource "azurerm_virtual_machine_scale_set" "minha_maquina_virtual" {
   }
 }
 
+resource "azurerm_network_interface" "interface_rede_mysql" {
+  name                = "interface-rede-mysql"
+  location            = azurerm_resource_group.meu_grupo_de_recursos.location
+  resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_interna.id
+    public_ip_address_id          = azurerm_public_ip.mysql_PIP.id
+    private_ip_address_allocation = "Dynamic"
+#    private_ip_address            = "10.0.2.3"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "mysql_machine" {
+  name                = "mysql-machine"
+  resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
+  location            = azurerm_resource_group.meu_grupo_de_recursos.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.interface_rede_mysql.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+}
+
 data "azurerm_public_ip" "meu_PIP" {
   name                = azurerm_public_ip.meu_PIP.name
   resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
@@ -146,4 +201,33 @@ output "public_ip_address" {
   value = data.azurerm_public_ip.meu_PIP.ip_address
 }
 
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_machine_scale_set
+data "azurerm_public_ip" "mysql_PIP" {
+  name                = azurerm_public_ip.mysql_PIP.name
+  resource_group_name = azurerm_resource_group.meu_grupo_de_recursos.name
+  depends_on          = [azurerm_linux_virtual_machine.mysql_machine,]
+}
+
+output "public_ip_address_mysql" {
+  value = data.azurerm_public_ip.mysql_PIP.ip_address
+}
+
+resource "null_resource" "para_uso" {
+  provisioner "remote-exec" {
+
+    # Para usar a chave é necessário descriptografá-la: openssl rsa -in id_rsa -out id_rsa.insecure
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install docker.io",
+      "sudo docker run -p 3306:3306 --name wordpress-mysql --restart always -e MYSQL_ROOT_PASSWORD=jhjggy$#@ -e MYSQL_DATABASE=wordpress -e MYSQL_USER=usr-wordpress -e MYSQL_PASSWORD=jhjggy$#@ -d mysql:5.7",
+    ]
+
+    connection {
+        host = data.azurerm_public_ip.mysql_PIP.ip_address
+        user = "adminuser"
+        type = "ssh"
+        private_key = file("~/.ssh/id_rsa.insecure")
+        timeout = "1m"
+        agent = true
+    }
+  }
+}
